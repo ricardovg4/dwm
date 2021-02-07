6cd53f594dbce7671641a999122780cfb58cdfe1
ricardovg4 <ricardo.velasco.guachalla@gmail.com>
2021-02-07 15:22:50 -0400

    font and bottom bar

--- config.h
+++ config.h
@@ -1,12 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const int topbar             = 0;        /* 0 means bottom bar */
+/* static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10"; */
+static const char *fonts[]          = { "Siji:size=10", "xos4 Terminus:size=10" };
+static const char dmenufont[]       = "xos4 Terminus:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -57,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-/*static const char bash_fish_pipe[] = "st -e bash -c 'exec fish'";*/
+/*static const char *termcmd[]  = { "st", "-e", "bash", "-c", "fish", NULL };*/
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
